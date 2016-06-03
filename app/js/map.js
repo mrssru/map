f95e5d1c1dc03404927d7810230008b49dc7b30a
Merzliakov Stanislav <mr.ss.ru@gmail.com>
2016-06-03 23:42:34 +0600

    fix some problems, add more contrast colors

--- app/js/map.js
+++ app/js/map.js
@@ -5,27 +5,26 @@ app.controller('mapCtrl', function($scope) {});
 app.directive('map', function() {
     return {
         restrict: 'E',
-        templateUrl: '/map/app/templates/map1.svg',
+        templateUrl: '/map/app/templates/map.svg',
         scope: {
             path: '@'
         },
         controller: function($scope, $location, $attrs) {
             var url = $attrs.path;
             $scope.getHref = function(value) {
-                var parent = $('map').parent();
-                parent.find('select option').each(function () {
-                    if ($(this).text().indexOf(value) >= 0) {
-                        $(this).attr('selected', 'selected');
-                    }
-                });
+                if(url === undefined) {
+                    $location.search("region", value);
+                } else {
+                    $location.url(url + "?region=" + value);
+                }
             };
 
             $scope.go = function (path) {
                 $location.path(path);
             };
             $scope.moved = function(event, name) {
-                $scope.x = event.clientX-parseInt($(".wrapper").offset().left,10);
-                $scope.y = event.pageY-175;
+                $scope.x = event.pageX;
+                $scope.y = event.pageY;
                 $scope.name = name;
             };
             $scope.show = false;
